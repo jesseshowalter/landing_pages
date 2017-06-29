6d89266a101510e10776c294cbc8580ae6caaf33
Jesse Showalter <iamjesseshowalter@gmail.com>
2017-06-28 17:41:04 -1000

    Initial file frame dump

---
+++ src/scripts/app.js
@@ -0,0 +1,128 @@
+// @codekit-prepend "vendors/jquery-3.1.1.min.js";
+// @codekit-prepend "vendors/slick.js";
+// @codekit-prepend "vendors/classie.js";
+// @codekit-prepend "vendors/modernizr.custom.js";
+// @codekit-prepend "vendors/lity.js";
+// @codekit-prepend "vendors/smoothState.js";
+
+$(document).ready(function(){
+
+
+  // -------------------------------
+  // vertical Tabs for Press and FAQ
+  // -------------------------------
+  $(".vertical-tab-content").hide();
+  $(".vertical-tab-content:first").show();
+
+  $(".vertical-tab").click(function(event) {
+    event.preventDefault();
+
+    $(".vertical-tab-content").hide();
+    var activeTab = $(this).attr("rel");
+    $("#"+activeTab).show();
+
+    $(".vertical-tab").removeClass("sideNavActive");
+    $(this).addClass("sideNavActive");
+  });
+
+  // -------------------------------
+  // Slider
+  // -------------------------------
+  $('.slider').slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 6500,
+    pauseOnHover: true,
+    arrows: false,
+  });
+  $('.flyerSlider').slick({
+    dots: true,
+    autoplay: false,
+    autoplaySpeed: 6500,
+    pauseOnHover: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: false,
+  });
+
+
+ // --------------------------------------------
+ //  Back To Top
+ // --------------------------------------------
+ //Click event to scroll to top
+ $('.vertical-tab').click(function(){
+   $('html, body').animate({scrollTop : 0},800);
+   return false;
+ });
+
+
+
+ // -------------------------------
+ // Mobile Navigation
+ // -------------------------------
+ (function() {
+  var triggerBttn = document.getElementById( 'nav-toggle-switch' ),
+    overlay = document.querySelector( 'div.overlay' ),
+    closeBttn = overlay.querySelector( 'button.overlay-close' );
+    transEndEventNames = {
+      'WebkitTransition': 'webkitTransitionEnd',
+      'MozTransition': 'transitionend',
+      'OTransition': 'oTransitionEnd',
+      'msTransition': 'MSTransitionEnd',
+      'transition': 'transitionend'
+    },
+    transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+    support = { transitions : Modernizr.csstransitions };
+
+  function toggleOverlay() {
+    if( classie.has( overlay, 'open' ) ) {
+      classie.remove( overlay, 'open' );
+      classie.add( overlay, 'close' );
+      var onEndTransitionFn = function( ev ) {
+        if( support.transitions ) {
+          if( ev.propertyName !== 'visibility' ) return;
+          this.removeEventListener( transEndEventName, onEndTransitionFn );
+        }
+        classie.remove( overlay, 'close' );
+      };
+      if( support.transitions ) {
+        overlay.addEventListener( transEndEventName, onEndTransitionFn );
+      }
+      else {
+        onEndTransitionFn();
+      }
+    }
+    else if( !classie.has( overlay, 'close' ) ) {
+      classie.add( overlay, 'open' );
+    }
+  }
+
+  triggerBttn.addEventListener( 'click', toggleOverlay );
+  closeBttn.addEventListener( 'click', toggleOverlay );
+ })();
+
+
+ // -------------------------------
+ // Smooth State
+ // -------------------------------
+    // (function ($) {
+    //  'use strict';
+    //  var $body    = $('html, body'), // Define jQuery collection
+    //      content  = $('#main').smoothState({
+    //        onStart : {
+    //          duration: 250,
+    //          render: function () {
+    //            content.toggleAnimationClass('is-exiting');
+    //
+    //            // Scroll user to the top
+    //            $body.animate({ 'scrollTop': 0 });
+    //
+    //          }
+    //        }
+    //      }).data('smoothState');
+    // })(jQuery);
+
+
+});
